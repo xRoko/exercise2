96c7e8dfb1f6166aebe62919bc76a9677913d525
Roman Kováč <67224950+xRoko@users.noreply.github.com>
2024-12-05 16:07:56 +0100

    Create simple-To-Do-List.js

---
+++ simple-To-Do-List.js
@@ -0,0 +1,11 @@
+/*
+Description: Build a terminal-based to-do list app.
+
+Features:
+Add tasks.
+View all tasks.
+Mark tasks as done or remove them.
+Focus: Use loops for displaying tasks and conditions for actions (e.g., checking if the task list is empty before showing tasks).
+*/
+
+const prompt = require('prompt-sync')();
