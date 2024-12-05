1ab3cd60618261cd3df1429248a3af56e0820e72
Roman Kováč <67224950+xRoko@users.noreply.github.com>
2024-12-05 16:14:31 +0100

    Create simple-calc.js

---
+++ simple-calc.js
@@ -0,0 +1,15 @@
+/*
+
+Description: Build a calculator that takes user input for basic operations.
+
+Features:
+Ask the user to input two numbers and an operator (+, -, *, /, ^2, abs, sqrt).
+Perform the calculation and display the result.
+Ask if the user wants to perform another calculation or exit.
+Focus: Conditionals for operator selection and loops for repeated use.
+
+Extra task: Create a calculator which is able to calculate quadratic equation e.x.: x^2 + x + 1 = 0
+*/
+
+const prompt = require('prompt-sync')();
+
