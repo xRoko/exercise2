c4ca890b89ba8537ff56693a6fc87ccbfb770c9e
Daniel Vala <daniel.vala@outlook.com>
2024-12-08 18:20:40 +0100

    Exercise 2,3,4 v1

--- simple-calc.js
+++ simple-calc.js
@@ -60,7 +60,7 @@ while (exit !== "yes") {
                     break;
             }
             console.log(`Result: ${result}`);
-            exit = "xy";
+            exit = "x";
             while (exit !== "yes" && exit !== "no") {
                 exit = prompt(`Do you want to exit (yes/no): `)
             }
