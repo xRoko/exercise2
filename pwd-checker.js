3a9b8bc90f4b008f5028b49d64eae1a7a974ecb6
Marek1 <marekdadak@seznam.cz>
2024-12-08 18:11:29 +0100

    Nemuzu odstranit chybu?

--- pwd-checker.js
+++ pwd-checker.js
@@ -12,6 +12,39 @@ Uppercase letters.
 Output a strength score or message ("Weak," "Moderate," "Strong").
 Focus: Use conditions to validate different password criteria and loops to analyze the string.
 
+Popis: Vyvinout nástroj pro kontrolu síly hesla.
+
+Vlastnosti:
+Vyzvěte uživatele k zadání hesla.
+Zkontrolujte:
+Minimální délka.
+Přítomnost čísel.
+Speciální znaky.
+Velká písmena.
+Zadejte skóre síly nebo zprávu („Slabá“, „Střední“, „Silná“).
+Zaměření: Použijte podmínky k ověření různých kritérií hesla a smyček pro analýzu řetězce.
 */
 
 const prompt = require('prompt-sync')();
+
+let password = prompt("Napis heslo ");
+
+// kontrola delky hesla
+       if(password.length > 13) {
+          console.log("velmi sylné heslo");
+       } else  if (password.length >= 4 && password.length >= 8 && password.length <= 13) {
+           console.log("Silne heslo");{
+           }
+           } else {
+           console.log("Stredni heslo");{
+           }
+           } else {
+           console.log("Slabé heslo");{
+           }
+
+
+
+
+
+}
+
