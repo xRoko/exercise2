2dec0979d57d9ad9dfd86ae6cbd30d8cbfbf2517
mkanuch <mkanuch@gmail.com>
2024-12-09 22:16:15 +0100

    cvicenie kalkulacka

--- pwd-checker.js
+++ pwd-checker.js
@@ -1,20 +1,3 @@
-/*
-
-Description: Develop a tool to check the strength of a password.
-
-Features:
-Prompt the user to enter a password.
-Check for:
-Minimum length.
-Presence of numbers.
-Special characters.
-Uppercase letters.
-Output a strength score or message ("Weak," "Moderate," "Strong").
-Focus: Use conditions to validate different password criteria and loops to analyze the string.
-
-*/
-
-
 const prompt = require('prompt-sync')();
 
 
