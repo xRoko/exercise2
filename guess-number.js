b5c2a45cefa4aba4e38c9beef114e2ad3fc04000
Andrej2610 <andrejapyro@seznam.cz>
2024-12-09 15:50:29 +0100

    my result

--- guess-number.js
+++ guess-number.js
@@ -12,3 +12,57 @@ Focus: Loops for repeated guesses and conditions for providing hints.
 */
 
 const prompt = require('prompt-sync')();
+
+const randomNumber = Math.floor(Math.random() * 100) + 1;
+
+console.log("Vítej ve hře");
+console.log("Hádej číslo od 1 do 100");
+console.log("Pokud to dáš na druhý pokus, jseš největší borec!");
+
+let pokus = 1;
+
+while (true) {
+
+    let input;
+
+    if (pokus === 1) {
+        input = prompt("Tak pojď - pokus první: ");
+
+        if (pokus === 2) {
+            console.log("Ty si vážně borec!");h
+        }
+
+    } else {
+        input = prompt(`Tak pojď - pokus číslo ${pokus}: `);
+    }
+
+    let hrac = parseInt(input, 10);
+
+    if (isNaN(hrac)) {
+
+        console.log("Prosím, zadej platné číslo!");
+        continue;
+    }
+
+    if (hrac === randomNumber) {
+
+        console.log(`Bravo!!! Uhodl jsi správné číslo na pokus číslo ${pokus}!`);
+        break; //
+
+    } else if (hrac > randomNumber) {
+        console.log("Je to příliš vysoké.");
+
+    } else {
+        console.log("Je to hodně nízké, ty joudo.");
+    }
+
+    if (pokus === 5) {
+        console.log("Ty seš ale kopyto!");
+    }
+
+    pokus++;
+}
+
+
+
+
