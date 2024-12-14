fc9e5de50fcdfb63cb2da52599a4a72376043a9f
Daniel Vala <daniel.vala@outlook.com>
2024-12-14 13:19:49 +0100

    calendar v1

--- calendar.js
+++ calendar.js
@@ -53,3 +53,224 @@ Events:
 */
 
 const prompt = require('prompt-sync')();
+let events = [
+    [
+        [""], [""], [""], [""], [""], [""], [""], [""], [""], [""],
+        [""], [""], [""], [""], [""], [""], [""], [""], [""], [""],
+        [""], [""], [""], [""], [""], [""], [""], [""], [""], [""],
+        [""]
+    ],
+    [
+        [""], [""], [""], [""], [""], [""], [""], [""], [""], [""],
+        [""], [""], [""], [""], [""], [""], [""], [""], [""], [""],
+        [""], [""], [""], [""], [""], [""], [""], [""], [""], [""],
+        [""]
+    ],
+    [
+        [""], [""], [""], [""], [""], [""], [""], [""], [""], [""],
+        [""], [""], [""], [""], [""], [""], [""], [""], [""], [""],
+        [""], [""], [""], [""], [""], [""], [""], [""], [""], [""],
+        [""]
+    ],
+    [
+        [""], [""], [""], [""], [""], [""], [""], [""], [""], [""],
+        [""], [""], [""], [""], [""], [""], [""], [""], [""], [""],
+        [""], [""], [""], [""], [""], [""], [""], [""], [""], [""],
+        [""]
+    ],
+    [
+        [""], [""], [""], [""], [""], [""], [""], [""], [""], [""],
+        [""], [""], [""], [""], [""], [""], [""], [""], [""], [""],
+        [""], [""], [""], [""], [""], [""], [""], [""], [""], [""],
+        [""]
+    ],
+    [
+        [""], [""], [""], [""], [""], [""], [""], [""], [""], [""],
+        [""], [""], [""], [""], [""], [""], [""], [""], [""], [""],
+        [""], [""], [""], [""], [""], [""], [""], [""], [""], [""],
+        [""]
+    ],
+    [
+        [""], [""], [""], [""], [""], [""], [""], [""], [""], [""],
+        [""], [""], [""], [""], [""], [""], [""], [""], [""], [""],
+        [""], [""], [""], [""], [""], [""], [""], [""], [""], [""],
+        [""]
+    ],
+    [
+        [""], [""], [""], [""], [""], [""], [""], [""], [""], [""],
+        [""], [""], [""], [""], [""], [""], [""], [""], [""], [""],
+        [""], [""], [""], [""], [""], [""], [""], [""], [""], [""],
+        [""]
+    ],
+    [
+        [""], [""], [""], [""], [""], [""], [""], [""], [""], [""],
+        [""], [""], [""], [""], [""], [""], [""], [""], [""], [""],
+        [""], [""], [""], [""], [""], [""], [""], [""], [""], [""],
+        [""]
+    ],
+    [
+        [""], [""], [""], [""], [""], [""], [""], [""], [""], [""],
+        [""], [""], [""], [""], [""], [""], [""], [""], [""], [""],
+        [""], [""], [""], [""], [""], [""], [""], [""], [""], [""],
+        [""]
+    ],
+    [
+        [""], [""], [""], [""], [""], [""], [""], [""], [""], [""],
+        [""], [""], [""], [""], [""], [""], [""], [""], [""], [""],
+        [""], [""], [""], [""], [""], [""], [""], [""], [""], [""],
+        [""]
+    ],
+    [
+        [""], [""], [""], [""], [""], [""], [""], [""], [""], [""],
+        [""], [""], [""], [""], [""], [""], [""], [""], [""], [""],
+        [""], [""], [""], [""], [""], [""], [""], [""], [""], [""],
+        [""]
+    ]
+];
+
+
+function menu() {
+    console.log(`MAIN MENU`)
+    console.log(`1. View Calendar\n2. Add Event\n3. View events\n4. Remove event\n5. Exit`);
+    let option = parseInt(prompt(`Choose an option: `));
+    while (isNaN(option) || option <= 0 || option > 5) {
+        option = parseInt(prompt(`Choose an option (1 - 5): `));
+    }
+    if (option === 1) {
+        console.log();
+        viewCalendar();
+        return true;
+    } else if (option === 2) {
+        console.log();
+        addEvent();
+        return true;
+    }
+    else if (option === 3) {
+        console.log();
+
+        viewEvents();
+        return true;
+    } else if (option === 4) {
+        console.log();
+        removeEvent();
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function viewCalendar() {
+    console.log(`VIEW CALENDAR`);
+    let month;
+    month = parseInt(prompt(`Choose month (1 - 12): `));
+    while (isNaN(month) || month <=0 || month > 12) {
+        month = parseInt(prompt(`Choose month (1 - 12): `));
+    }
+    process.stdout.write(`Calendar for ${months[month - 1]} 2024:`);
+    for (let i = 0; i < daysInMonth[month - 1]; i++) {
+        if (i % 7 === 0) {
+            console.log();
+            process.stdout.write(`${i + 1} `);
+        } else if (i < 10) {
+            process.stdout.write(` ${i + 1} `);
+        } else {
+            process.stdout.write(`${i + 1} `);
+        }
+    }
+    console.log(`\n`);
+}
+
+function addEvent() {
+    console.log(`ADD NEW EVENT`);
+    let newEvent = prompt(`New event: `);
+    let newEventMonth = parseInt(prompt(`New event month: `));
+    while (isNaN(newEventMonth) || newEventMonth <= 0 || newEventMonth > 12) {
+        newEventMonth = parseInt(prompt(`New event month (1 - 12): `));
+    }
+    let newEventDay = parseInt(prompt(`New event day: `));
+    while (isNaN(newEventDay) || newEventDay <= 0 || newEventDay > daysInMonth[newEventMonth - 1]) {
+        newEventDay = parseInt(prompt(`New event day ( 1 - ${daysInMonth[newEventMonth - 1]}): `));
+    }
+    if (events[newEventMonth - 1][newEventDay - 1][0] === "") {
+        events[newEventMonth - 1][newEventDay - 1][0] = newEvent;
+    } else {
+        events[newEventMonth - 1][newEventDay - 1].push(newEvent);
+    }
+    console.log();
+}
+
+function viewEvents() {
+    console.log(`VIEW EVENTS`);
+    let viewEvents = prompt(`To view events choose a month (1 - 12) or type all: `);
+    if (viewEvents === "all") {
+        let indexMonth = 0;
+        for (let month of events) {
+            indexMonth++;
+            let indexDay = 0;
+            for (let day of month) {
+                indexDay++;
+                if (day[0] !== "") {
+                    for (let event of day) {
+                        console.log(`${indexDay}.${indexMonth} - ${event}.`);
+                    }
+                }
+            }
+        }
+    } else if (viewEvents >= 1 && viewEvents <= 12) {
+        let indexDay = 0;
+        for (let day of events[viewEvents - 1]) {
+            indexDay++;
+            if (day[0] !== "") {
+                for (let event of day) {
+                    console.log(`${indexDay}.${viewEvents} - ${event}.`);
+                }
+            }
+        }
+    }
+    console.log();
+}
+
+function removeEvent() {
+    console.log(`REMOVE EVENTS`);
+    console.log(`To delete an event, select date: `)
+    let deleteMonth = parseInt(prompt(`Choose month (1 - 12): `));
+    while (isNaN(deleteMonth) || deleteMonth <=0 || deleteMonth > 12) {
+        deleteMonth = parseInt(prompt(`Choose month (1 - 12): `));
+    }
+    let deleteDay = parseInt(prompt(`Choose day (1 - ${daysInMonth[deleteMonth - 1]}): `));
+    while (isNaN(deleteDay) || deleteDay <=0 || deleteDay > daysInMonth[deleteMonth - 1]) {
+        deleteDay = parseInt(prompt(`Choose day (1 - ${daysInMonth[deleteMonth - 1]}): `));
+    }
+
+    if (events[deleteMonth - 1][deleteDay - 1][0] === "") {
+        console.log(`No events for selected date.`);
+    } else if (events[deleteMonth - 1][deleteDay - 1].length > 1) {
+        console.log(`There is more than one event for this day, select one to delete: `);
+        let index = 0;
+        for (let event of events[deleteMonth - 1][deleteDay - 1]) {
+            index++;
+            console.log(`${index}) ${event}.`);
+        }
+        let deleteIndex = parseInt(prompt(`Choose event to delete: `));
+        while (isNaN(deleteIndex) || deleteIndex <=0 || deleteIndex > index) {
+            deleteIndex = parseInt(prompt(`Choose event to delete (1 - ${index}): `));
+        }
+        events[deleteMonth - 1][deleteDay - 1].splice(deleteIndex - 1, 1);
+    } else {
+        events[deleteMonth - 1][deleteDay - 1][0] = "";
+    }
+    console.log();
+}
+
+// Application Terminal Calendar
+let daysInMonth = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+console.log(`Welcome to the Terminal Calendar!`);
+let appRun = true;
+while (appRun) {
+    appRun = menu();
+}
+
+
+
