23da1f24b3a1379e00f9584ccb682c6157f897bf
mkanuch <mkanuch@gmail.com>
2024-12-16 22:05:43 +0100

    uprava bez result - priamo console.log

--- simple-calc.js
+++ simple-calc.js
@@ -9,41 +9,37 @@ while (continueCalculation) {
     let operator = prompt('Please, enter required operator (+, -, *, /, ^2, abs, sqrt): ').trim();
 
     console.log('');
-    let result = '';
-
 
     if (operator === '+') {
-        result = `Addition: ${number_a} + ${number_b} = ${number_a + number_b}`;
+        console.log(`Addition: ${number_a} + ${number_b} = ${number_a + number_b}`);
 
     } else if (operator === '-') {
-        result = `Subtraction: ${number_a} - ${number_b} = ${number_a - number_b}`;
+        console.log(`Subtraction: ${number_a} - ${number_b} = ${number_a - number_b}`);
 
     } else if (operator === '*') {
-        result = `Multiplication: ${number_a} * ${number_b} = ${number_a * number_b}`;
+        console.log(`Multiplication: ${number_a} * ${number_b} = ${number_a * number_b}`);
 
     } else if (operator === '/') {
         if (number_b === 0) {
-            result = 'Division by zero is not allowed.';
+            console.log('Division by zero is not allowed.');
         } else {
-            result = `Division: ${number_a} / ${number_b} = ${(number_a / number_b).toFixed(2)}`;
+            console.log(`Division: ${number_a} / ${number_b} = ${(number_a / number_b).toFixed(2)}`);
         }
     } else if (operator === '^2') {
-        result = `Square of ${number_a} = ${number_a * number_a}\nSquare of ${number_b} = ${number_b * number_b}`;
+        console.log(`Square of ${number_a} = ${number_a * number_a}\nSquare of ${number_b} = ${number_b * number_b}`);
 
     } else if (operator === 'abs') {
-        result = `Absolute value of ${number_a} = ${Math.abs(number_a)}\nAbsolute value of ${number_b} = ${Math.abs(number_b)}`;
+        console.log(`Absolute value of ${number_a} = ${Math.abs(number_a)}\nAbsolute value of ${number_b} = ${Math.abs(number_b)}`);
 
     } else if (operator === 'sqrt') {
         if (number_a < 0) {
-            result = 'Square root of negative number is not allowed.';
+            console.log('Square root of negative number is not allowed.');
         } else {
-            result = `Square root of ${number_a} = ${Math.sqrt(number_a).toFixed(2)}\nSquare root of ${number_b} = ${Math.sqrt(number_b).toFixed(2)}`;
+            console.log(`Square root of ${number_a} = ${Math.sqrt(number_a).toFixed(2)}\nSquare root of ${number_b} = ${Math.sqrt(number_b).toFixed(2)}`);
         }
     } else {
-        result = 'Invalid operator. Please try again.';
+        console.log('Invalid operator. Please try again.');
     }
-
-    console.log(result);
 
     let userChoice = prompt('Do you want to continue? y/n: ');
     if (userChoice !== 'y') {
